4c928651fe18e3a9c2dd30614002ddeb86bb764f
Matthew <matthewxd1998@gmail.com>
2022-10-19 20:02:15 -0700

    Deleted test.py. Adding content to Chapter 2 notebook. Adding Chapter 2 scripts of according to each part of the YouTube video. https://www.youtube.com/watch?v=EBml3l5uxNI&list=PLWFlMBumSLSaqgYlK2wD7XSPRQ2p-_Tl4&index=3&ab_channel=PhysicsExplained

---
+++ Python_Scripts/Chapter_2_task2.py
@@ -0,0 +1,33 @@
+from vpython import *
+
+#x0 = 0m, t0 = 0s, v = 0.45m/s, a = 0.5m/s^2, where is the cart after 1.5s
+#Break it in to small steps,dt=0.01
+
+g1 = graph(title="Distance vs. Time", xtitle='Time (s)', ytitle='Distance (m)', fast=True)
+x1 = gcurve(color=color.blue, markers=False)
+g2 = graph(title="Velocity vs. Time", ytitle= 'Velocity (m/s)', xtitle='Time (s)', fast=True)
+v1 = gcurve(color=color.red, markers=False)
+
+x = 0
+t = 0
+v = 0.45
+a = 0.5
+dt = 0.01
+i = 0
+
+x1.plot(t,x)
+v1.plot(t,v)
+
+while t <= 1.5:
+    print("x = {} m at t = {} s".format(x, t))
+    if t < 1.5:
+        x = x + v*dt + (1/2)*a*dt**2
+        t += dt
+        v += a*dt
+        i +=1
+        x1.plot(t,x)
+        v1.plot(t,v)
+    else:
+        break
+            
+print(i)
